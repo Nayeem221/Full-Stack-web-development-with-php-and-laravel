d4aa600db68a8acda33aebf0efacb1bf042e23e3
Nayeem <103898592+Nayeem221@users.noreply.github.com>
2025-12-06 22:58:10 +0600

    commit

---
+++ If else statement,string concatanation,function and object/script.js
@@ -0,0 +1,75 @@
+// function f(number) {
+     
+//      if(number<0)
+//        return 'negative'     //write a function to check wheather its negative positive or zero//
+//      else if(number>0)
+//        return 'positive'
+// else
+//       return 'zero'
+     
+    
+// }
+// let result=f(56)
+// console.log(result);
+
+// function check(number) {
+//     if(number%2===0)
+//         return 'even'
+//     else
+//         return 'odd'            //write a function to check  even odd
+    
+// }
+// let cheaker=check(47)
+// console.log(cheaker);
+
+
+// function square(s) {
+//     return s*s
+                          //write a javascript function to calculate the square
+// }
+// let result=square(7)
+// console.log(result);
+
+//string concatanation
+// let str1='nayeem'
+// let str2='ridoy'
+// let str3=str1 + ' '+str2       //string concatanation
+// console.log(str3);
+// let str1='nayeem'
+// str1='nayeem'+' ';
+// let str2='ridoy'
+
+// let str3=str1.concat(str2)       //string concatanation
+// console.log(str3);
+
+
+// function number(a,b,c) {
+//     if(a>b&&a>c)
+//     return `a is greater and value of a is = ${a}`
+// else if(b>a&&b>c){
+// return `b is greater and value of b is = ${b}`             //find the biggest number between a,b,c
+
+// }
+// else return  `c is greater and value of c is = ${c}`
+// }
+// let result=number(90,76,45)
+// console.log(result);
+
+
+// object
+
+// const student={
+//     name:'nayeem',
+//     id:34234,
+//     department:'cse',
+//     getdata:function(){
+//         console.log(`the student name is ${this.name} and the age of the student is ${this.id}`);
+//     },
+//     address:{
+//         city:'dhaka',
+//         postcode:232323,
+//         phone:233223232323,
+        
+//     }
+// }
+// console.log(student.getdata());
